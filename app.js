d734754acdb114992c646a68c6a055531f3830bc
Levy-Naibei <levynaibei2@gmail.com>
2020-09-22 23:15:50 +0300

    feat: handles cors policy

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
+// express instance
 const app = express();
 
+// loging req, res to the terminal
 app.use(morgan('dev'));
+
+/**
+ * parsing the body of incoming req because 
+ * it may not be in the format thats readable in node 
+ * also handle cors policy; so that other web pages/clients can access API
+ */
+ app.use(express.urlencoded({ extended: false}));
+ app.use(express.json());
+ app.use(cors());
+
 
 // define routes
 app.use('/products', require('./api/routes/products'));
